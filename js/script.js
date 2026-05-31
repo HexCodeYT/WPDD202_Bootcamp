2b2c6158e3cbe90bf1b03a0077aba92ea31f9640
Pawan Sedara <pawansedara321@gmail.com>
2026-05-31 15:30:18 +1000

    Add service card zoom controls

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 const clickButton = document.getElementById("clickButton");
 
 clickButton.addEventListener("click", () => {
-    alert("Thank you for visiting Skillup Labs");
+  alert("Thank you for visiting Skillup Labs");
+});
+
+const serviceCards = document.querySelector(".service-cards");
+const zoomInBtn = document.getElementById("zoomInBtn");
+const zoomOutBtn = document.getElementById("zoomOutBtn");
+const zoomLevelText = document.getElementById("zoomLevel");
+
+let scale = 1;
+
+zoomInBtn.addEventListener("click", () => {
+  if (scale < 1.5) {
+    scale += 0.1;
+
+    serviceCards.style.transform = `scale(${scale})`;
+    zoomLevelText.textContent = `${Math.round(scale * 100)}%`;
+  }
+});
+
+zoomOutBtn.addEventListener("click", () => {
+  if (scale > 0.7) {
+    scale -= 0.1;
+
+    serviceCards.style.transform = `scale(${scale})`;
+    zoomLevelText.textContent = `${Math.round(scale * 100)}%`;
+  }
 });
